a16c97137db5f3b20b2ffec90d625ff5f074b051
leo chan <leochan007@163.com>
2021-06-17 14:18:09 +0800

    update sdk remove useless files.

--- src/hooks/useCustomDCEffect.ts
+++
@@ -1,5 +0,0 @@
-import { useDeepCompareEffect } from "react-use"
-
-export function useCustomDCEffect(action: any, deps: any[] | undefined | null) {
-    useDeepCompareEffect(action, [deps || {}])
-}
