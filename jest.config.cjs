c057dc1d4b1e63caeea86f8fe0a2efff1c74ce4f
Winda <windatang@gmail.com>
2023-08-01 07:40:30 +0000

    jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,13 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/dist'],
-  collectCoverage: true,
   verbose: true,
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-  },
-  moduleNameMapper: {
-    '\\.(css|scss|less)$': '<rootDir>/test/styleMock.js',
-  },
+    '^.+\\.(js|jsx|ts|tsx)$': './node_modules/babel-jest',
+  }
 }
