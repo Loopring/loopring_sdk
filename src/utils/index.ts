ae8a06e7765602495b40da01274eb9593b4a47b8
john.li <john.li@loopring.io>
2024-01-19 11:35:49 +0800

    wip

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,6 @@ export * from "./network_tools";
 export * from "./symbol_tools";
 export * from "./formatter";
 export * from "./swap_calc_utils";
+import activation from "./activation";
+export {activation}
 export type RequiredPart<T, K> = Required<{ [K in keyof T]: Pick<T, K> }> & T;
