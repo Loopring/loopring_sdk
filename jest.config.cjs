239de527a39de1034972c48fa16f544377fdef39
john.li <john.li@loopring.io>
2024-09-19 17:22:38 +0800

    WIP

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   verbose: true,
   transform: {
+    // '^.+\\.(ts|tsx)?$': 'ts-jest',
+    // '^.+\\.(js|jsx)$': 'babel-jest',
+    // '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.(js|jsx|ts|tsx)$': './node_modules/babel-jest',
+
   },
   "moduleNameMapper": {
     "axios": "axios/dist/node/axios.cjs"
